923dd5141e31aa75150a2e898251abdd6e17af7d
Chupakhin Arthur <rezident.artom@yandex.ru>
2025-12-31 13:18:05 +0000

    Update 2 files
    - /sberhivetask3/run.sh
    - /sberhivetask3/query.sql

---
+++ sberhivetask3/query.sql
@@ -0,0 +1,10 @@
+USE chupahinar;
+
+SELECT 
+    i.region,
+    SUM(IF(u.sex = 'male', 1, 0)) AS male,
+    SUM(IF(u.sex = 'female', 1, 0)) AS female
+FROM logs l
+JOIN ipregions i ON i.ip = l.ip
+JOIN users u ON u.ip = l.ip
+GROUP BY i.region;
