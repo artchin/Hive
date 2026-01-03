f8b56dd66d1d95703f0c77010bf46c047e73c72f
Chupakhin Arthur <rezident.artom@yandex.ru>
2026-01-03 08:29:43 +0000

    Update file query.sql

--- sberhivetask4/query.sql
+++ sberhivetask4/query.sql
@@ -1,7 +1,7 @@
 use chupahinar;
 
-SELECT TRANSFORM (ip, url, size, code, browser, date)
+SELECT TRANSFORM (ip, url, size, code, browser, date_time)
 USING 'sed "s/\.ru\//.com\//"'
-AS (ip, url, size, code, browser, date)
+AS (ip, url, size, code, browser, date_time)
 from logs 
 limit 10;
