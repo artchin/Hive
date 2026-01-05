54aba36277fd8f2324e9ba0dbf1c263de058c737
Chupakhin Arthur <rezident.artom@yandex.ru>
2026-01-05 15:19:41 +0000

    Update 2 files
    - /sberhivetask5/run.sh
    - /sberhivetask5/query.sql

---
+++ sberhivetask5/query.sql
@@ -0,0 +1,36 @@
+USE chupahinar;
+
+set hive.exec.parallel=True;
+set hive.exec.parallel.thread.number=8;
+set mapreduce.job.reduces=30;
+
+
+-all
+SELECT 
+    i.region,
+    SUM(IF(u.sex = 'male', 1, 0)) AS male,
+    SUM(IF(u.sex = 'female', 1, 0)) AS female
+FROM logs l
+JOIN ipregions i ON i.ip = l.ip
+JOIN users u ON u.ip = l.ip
+GROUP BY i.region;
+
+--1k
+SELECT 
+    i.region,
+    SUM(IF(u.sex = 'male', 1, 0)) AS male,
+    SUM(IF(u.sex = 'female', 1, 0)) AS female
+FROM logs l TABLESAMPLE (1000 ROWS) l
+JOIN ipregions i ON i.ip = l.ip
+JOIN users u ON u.ip = l.ip
+GROUP BY i.region;
+
+--10k
+SELECT 
+    i.region,
+    SUM(IF(u.sex = 'male', 1, 0)) AS male,
+    SUM(IF(u.sex = 'female', 1, 0)) AS female
+FROM logs l TABLESAMPLE (10000 ROWS) l
+JOIN ipregions i ON i.ip = l.ip
+JOIN users u ON u.ip = l.ip
+GROUP BY i.region;
