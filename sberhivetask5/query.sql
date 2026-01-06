dce2861c8b20fd759a23cdd5838845fcc0bfe648
Chupakhin Arthur <rezident.artom@yandex.ru>
2026-01-06 08:44:47 +0000

    Update file query.sql

--- sberhivetask5/query.sql
+++ sberhivetask5/query.sql
@@ -5,32 +5,38 @@ set hive.exec.parallel.thread.number=8;
 set mapreduce.job.reduces=30;
 
 
---all
+--10 ROWS
 SELECT 
     i.region,
     SUM(IF(u.sex = 'male', 1, 0)) AS male,
     SUM(IF(u.sex = 'female', 1, 0)) AS female
-FROM logs l
+FROM logs TABLESAMPLE (10 ROWS) l
 JOIN ipregions i ON i.ip = l.ip
 JOIN users u ON u.ip = l.ip
-GROUP BY i.region;
+GROUP BY i.region
+ORDER BY male DESC
+LIMIT 5;
 
---1k
+--50 ROWS
 SELECT 
     i.region,
     SUM(IF(u.sex = 'male', 1, 0)) AS male,
     SUM(IF(u.sex = 'female', 1, 0)) AS female
-FROM logs TABLESAMPLE (1000 ROWS) l
+FROM logs TABLESAMPLE (50 ROWS) l
 JOIN ipregions i ON i.ip = l.ip
 JOIN users u ON u.ip = l.ip
-GROUP BY i.region;
+GROUP BY i.region
+ORDER BY male DESC
+LIMIT 5;
 
---10k
+--100 ROWS
 SELECT 
     i.region,
     SUM(IF(u.sex = 'male', 1, 0)) AS male,
     SUM(IF(u.sex = 'female', 1, 0)) AS female
-FROM logs TABLESAMPLE (10000 ROWS) l
+FROM logs TABLESAMPLE (100 ROWS) l
 JOIN ipregions i ON i.ip = l.ip
 JOIN users u ON u.ip = l.ip
-GROUP BY i.region;
+GROUP BY i.region
+ORDER BY male DESC
+LIMIT 5;
